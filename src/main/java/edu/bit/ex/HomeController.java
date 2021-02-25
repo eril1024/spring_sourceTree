52c3e0c746e2542397b4423b6a5e53f8dd24c092
eril1024 <eril1024@gmail.com>
2021-02-25 10:51:48 +0900

    #6

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//#2 수정
 /**
  * Handles requests for the application home page.
  */
